e69c756b9d54755f449877b5edae1546083d668c
MChang36 <matthew36chang@gmail.com>
2023-03-21 18:17:27 -0500

    Improved expectation calc

--- trading_algo.py
+++ trading_algo.py
@@ -1,15 +1,18 @@
-from typing import Dict, List
-from datamodel import OrderDepth, TradingState, Order
-import math
-import pandas as pd
-import numpy as np
+from typing import Any, Dict, List
+from datamodel import OrderDepth, TradingState, Order, Symbol
+#import math
+#import pandas as pd
+#import numpy as np
 import statistics
 
 class Trader:
-    limits = {"PEARLS": 20, "BANANAS": 20}
     
     def __init__(self):
-        self.history = {"PEARLS": {}, "BANANAS": {}}
+        self.history = {"PEARLS": [], "BANANAS": []}
+        self.limits = {"PEARLS": 20, "BANANAS": 20}
+
+    def get_limit(self, product):
+        return self.limits[product]
 
     def calc_expected(self, state: TradingState):
         expectations = {}
@@ -27,19 +30,17 @@ class Trader:
             for price in order_depth.buy_orders:
                 bid_hist.append([price]*order_depth.buy_orders[price])
             bid_hist = [item for sublist in bid_hist for item in sublist]
-            self.history[product].setdefault("BID",[]).append(bid_hist)
-            self.history[product].setdefault("ASK",[]).append(ask_hist)
-        
+            
+            print(statistics.median(bid_hist+ask_hist))
+            
+            self.history[product].append(statistics.median(bid_hist+ask_hist))
+            
             # Calc average of up to last 10 timestamps
             lookback = 100
-            bid_sample = self.history[product]["BID"][-lookback:]
-            bid_exp = statistics.mean([item for sublist in bid_sample for item in sublist])
-            ask_sample = self.history[product]["ASK"][-lookback:]
-            ask_exp = statistics.mean([item for sublist in ask_sample for item in sublist])
-            expectations[product] = (bid_exp, ask_exp)
+            sample = self.history[product][-lookback:]
+            expectations[product] = statistics.mean(sample)
             
         return expectations
-    
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
         result = {}
@@ -49,32 +50,33 @@ class Trader:
         for product in state.listings.keys():
             order_depth: OrderDepth = state.order_depths[product]
             orders: list[Order] = []
+            if product in state.position.keys():
+                print("Position:", product, state.position[product])
 
             # buy
-            ask = sorted([price for price in order_depth.sell_orders.keys() if price < expectations[product][1]])
+            ask = sorted([price for price in order_depth.sell_orders.keys() if price < expectations[product]])
             ask_volumes = [order_depth.sell_orders[price] for price in ask]
             if product in state.position.keys():
                 limit = self.limits[product] - state.position[product]
             else:
                 limit = self.limits[product]
-
             while len(ask) > 0:
                 if limit + ask_volumes[0] > 0:
-                    print("BUY", str(-ask_volumes[0])+"x", ask[0])
+                    print("BUY", str(-ask_volumes[0])+"x", product, ask[0])
                     orders.append(Order(product, ask[0], -ask_volumes[0]))
-                else:
+                elif limit > 0:
                     # put in order with remaining limit
-                    print("BUY", str(limit)+"x", ask[0])
+                    print("BUY", str(limit)+"x", product, ask[0])
                     orders.append(Order(product, ask[0], limit))
                 best_ask = ask.pop(0)
                 best_vol = ask_volumes.pop(0)
                 limit += best_vol
             if limit > 0:
-                print("BUY", str(limit)+"x", 0.8*expectations[product][1])
-                orders.append(Order(product, 0.8*expectations[product][1], limit))
+                print("BUY", str(limit)+"x", product, 0.9*expectations[product])
+                orders.append(Order(product, 0.9*expectations[product], limit))
 
             #sell
-            bid = sorted([price for price in order_depth.buy_orders.keys() if price > expectations[product][0]])
+            bid = sorted([price for price in order_depth.buy_orders.keys() if price > expectations[product]])
             bid_volumes = [order_depth.buy_orders[price] for price in bid]
             if product in state.position.keys():
                 limit = self.limits[product] + state.position[product]
@@ -82,18 +84,18 @@ class Trader:
                 limit = self.limits[product]
             while len(bid) > 0:
                 if limit - bid_volumes[0] > 0:
-                    print("SELL", str(bid_volumes[0])+"x", bid[0])
+                    print("SELL", str(bid_volumes[0])+"x", product, bid[0])
                     orders.append(Order(product, bid[0], -bid_volumes[0]))
-                else:
+                elif limit > 0:
                     # put in order with remaining limit
-                    print("SELL", str(limit)+"x", bid[0])
+                    print("SELL", str(limit)+"x", product, bid[0])
                     orders.append(Order(product, bid[0], -limit))
                 best_bid = bid.pop(0)
                 best_vol = bid_volumes.pop(0)
-                limit += best_vol
+                limit -= best_vol
             if limit > 0:
-                print("SELL", str(limit)+"x", 1.2*expectations[product][1])
-                orders.append(Order(product, 1.2*expectations[product][1], -limit))
+                print("SELL", str(limit)+"x", product, 1.1*expectations[product])
+                orders.append(Order(product, 1.1*expectations[product], -limit))
 
             result[product] = orders
         return result
